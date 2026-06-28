fa024b7870e5da4f7d89d86a13411e05b12e565f
Tanay <tanayagarwaldec028@gmail.com>
2026-06-28 16:48:01 +0530

    Initial working digital lock system

---
+++ door_lock.v
@@ -0,0 +1,90 @@
+module door_lock(input clk,input reset,input [3:0]password_entry,input enter,output unlock,output ALARM);
+
+parameter s0=4'd0,s1=4'd1,s2=4'd2,s3=4'd3;
+parameter attempts=4'd3;
+parameter hold=4'd10;
+parameter alarm_time=4'd10;
+parameter password=4'b1100;
+
+reg [3:0]state,next_state;
+reg [3:0]count,timer,alaarm;
+wire correct;
+
+assign correct = (password_entry==password);
+
+always @(posedge clk)begin
+    if(reset)
+    state<=s0;
+    else
+    state<=next_state;
+end
+
+always @(posedge clk) begin
+if(reset)
+count<=4'd0;
+else if(state==s2 && count!=attempts)
+count<=count+1;
+else if(state==s1)
+count<=0;
+else if(state==s3 && alaarm==alarm_time)
+count<=0;
+end
+
+always @(posedge clk) begin
+    if(reset)
+    timer<=0;
+    else if(state==s1 && timer!=hold)
+    timer<=timer+1;
+    else
+    timer<=0;
+end
+
+always @(posedge clk) begin
+    if(reset)
+    alaarm<=0;
+    else if(state==s3 && alaarm!=alarm_time)
+    alaarm<=alaarm+1;
+    else
+    alaarm<=0;
+end
+
+
+always @(*) begin
+    next_state=state;
+    case(state) 
+   s0:begin
+    if(correct && enter)
+    next_state=s1;
+    else if(enter && !correct)
+    next_state=s2;
+   end
+
+    s2:begin
+        if(count==attempts-1)
+        next_state=s3;
+        else
+        next_state=s0;
+    end
+
+    s1:begin
+        if(timer!=hold)
+        next_state=s1;
+        else 
+        next_state=s0;
+    end
+
+    s3: begin
+        if(alaarm!=alarm_time)
+        next_state=s3;
+        else
+        next_state=s0;
+    end
+
+    default:
+        next_state=s0;
+endcase
+end
+
+assign unlock=(state==s1);
+assign ALARM=(state==s3);
+endmodule
---
+++ door_lock_tb.v
@@ -0,0 +1,54 @@
+module door_lock_tb;
+
+door_lock dut(.clk(clk),.reset(reset),.password_entry(password_entry),.enter(enter),.unlock(unlock),.ALARM(ALARM));
+
+reg [3:0]password_entry;
+reg enter,clk,reset;
+ wire ALARM,unlock;
+
+ initial begin
+    clk=0;
+    forever begin
+    #5 clk=~clk;
+    end
+ end
+
+ initial begin
+    reset=1;
+    #12 reset=0;
+ end
+
+ initial begin
+enter=0;
+password_entry=0;
+    #10
+    password_entry=4'b0101;
+    enter=1;
+    #10
+    enter=0;
+    #10
+    password_entry=4'b1100;
+    enter=1;
+    #10 enter=0;
+    #10 
+    password_entry=4'b1001;
+    enter=1;
+    #10 enter=0;
+    #10 
+    password_entry=4'b1000;
+    enter=1;
+    #10 enter=0;
+    #70 
+    password_entry=4'b1101;
+    enter=1;
+    #10 enter=0;
+
+    #200 $finish;
+    end
+
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,door_lock_tb);
+    $monitor("Time=%0t,reset=%b,password_entry=%b,enter=%b,ALARM=%b,unlock=%b,count=%d,timer=%d,alaarm=%d",$time,reset,password_entry,enter,ALARM,unlock,dut.count,dut.timer,dut.alaarm);
+ end
+ endmodule
